e778672af927a4ff85f3162b7639ab9ff3bfb32d
Laurent Mouha <laurent.mouha@cegeka.be>
2015-06-16 10:53:05 +0200

    Added register and unregister functionality

--- web/modules/custom/matukio_user/src/Controller/UserController.php
+++ web/modules/custom/matukio_user/src/Controller/UserController.php
@@ -8,6 +8,8 @@
 namespace Drupal\matukio_user\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Class UserController.
@@ -16,29 +18,127 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class UserController extends ControllerBase {
   /**
-   * Hello.
-   *
-   * @return string
-   *   Return Hello string.
+   * Register a user
+   * @param $id : the id of the event
+   * @param $name : the name to register
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function register($id, $name) {
-    return [
-        '#type' => 'markup',
-        '#markup' => $this->t('Hello @name to @event!', ['@name' => $name, '@event' => $id])
-    ];
+    $node = Node::load($id);
+    $params = array();
+    if ($node && $node->getType() == 'event') {
+      $this->matukio_remove_unused_values_from_field($node, 'field_event_members');
+      $values = $node->get('field_event_members')->getValue();
+
+      if (!$this->matukio_user_is_registered($name, $values)) {
+        $node->get('field_event_members')->appendItem($name);
+        $node->save();
+        $params['response_text'] = $this->t('Hello @name, welcome to @event!', [
+          '@name' => $name,
+          '@event' => $node->getTitle(),
+        ]);
+        $params['response'] = '1';
+      }
+      else {
+        $params['response_text'] = $this->t('@name already is registered for @event!', [
+          '@name' => $name,
+          '@event' => $node->getTitle(),
+        ]);
+        $params['response'] = '2';
+      }
+    }
+    else {
+      $params['response'] = '0';
+      $params['response_text'] = $this->t("Error: couldn't find event with id @id", [
+        '@id' => $id
+      ]);
+    }
+    return new JsonResponse($params);
   }
 
   /**
-   * Hello.
-   *
-   * @return string
-   *   Return Hello string.
+   * Unregister a user
+   * @param $id : the event id
+   * @param $name : the name of the user
+   * @return array
    */
   public function unregister($id, $name) {
-    return [
-        '#type' => 'markup',
-        '#markup' => $this->t('Hello @name!', ['@name' => $name])
-    ];
+    $node = Node::load($id);
+    $params = array();
+    if ($node && $node->getType() == 'event') {
+      $this->matukio_remove_unused_values_from_field($node, 'field_event_members');
+
+      if ($this->matukio_user_is_registered($name, $values)) {
+        $values = $node->get('field_event_members')->getValue();
+        foreach ($values as $key => $value) {
+          if ($value['value'] == $name) {
+            $index = $key;
+          }
+        }
+
+        $node->get('field_event_members')->removeItem($index);
+        $node->updateOriginalValues();
+        $node->save();
+        $params['response_text'] = $this->t('@name is unregistered from @event!', [
+          '@name' => $name,
+          '@event' => $node->getTitle(),
+        ]);
+        $params['response'] = '1';
+      }
+      else {
+        $params['response_text'] = $this->t('@name was not registered for @event!', [
+          '@name' => $name,
+          '@event' => $node->getTitle(),
+        ]);
+        $params['response'] = '2';
+      }
+    }
+    else {
+      $params['response'] = '0';
+      $params['response_text'] = $this->t("Error: couldn't find event with id @id", [
+        '@id' => $id
+      ]);
+    }
+    return new JsonResponse($params);
   }
 
+  /**
+   * Check if a user is registered in the values from a field
+   * @param $name
+   * @param $values
+   * @return bool
+   */
+  protected function matukio_user_is_registered($name, $values) {
+    foreach ($values as $value) {
+      if ($value['value'] == $name) {
+        return TRUE;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Clear empty lines from multi value field. Use at own risk.
+   * @param \Drupal\node\Entity\Node $node
+   * @param $field
+   */
+  public function matukio_remove_unused_values_from_field(Node &$node, $field) {
+    $values = $node->get($field)->getValue();
+    $filtered = array();
+    foreach ($values as $value) {
+      if (!empty($value['value'])) {
+        $filtered[] = $value['value'];
+      }
+    }
+
+    if (count($filtered) !== count($values)) {
+      for ($a = 0; $a < count($values); $a++) {
+        $node->get($field)->removeItem(0);
+      }
+      foreach ($filtered as $item) {
+        $node->get($field)->appendItem($item);
+      }
+      $node->save();
+    }
+  }
 }
