1f85220320a26b6683229545c6fd48bf1f0fb88f
Laurent Mouha <laurent.mouha@cegeka.be>
2015-06-16 14:56:54 +0200

    Added validation to FeedbackController

--- web/modules/custom/matukio_feedback/src/Controller/FeedbackController.php
+++ web/modules/custom/matukio_feedback/src/Controller/FeedbackController.php
@@ -30,34 +30,24 @@ class FeedbackController extends ControllerBase {
     if ($node && $node->getType() == 'event') {
 
       $params = $request->request->all();
+      $this->feedback_check_required_fields($response, $params);
 
-      $new_page_values = array(
-        'type' => 'feedback',
-        'title' => 'feedback_' . time(),
-        'field_feedback_feedback' => array(
-          'value' => $params['feedback'],
-          'format' => 'basic_html',
-        ),
-        'field_feedback_user' => array(
-          'value' => $params['user'],
-        ),
-        'field_feedback_rating' => array(
-          'value' => $params['rating'],
-        ),
-      );
+      if (!empty($response)) {
+        return new JsonResponse($response);
+      }
 
-      $new_page = entity_create('node', $new_page_values);
-      $new_page->save();
+      $new_feedback = $this->feedback_create_feedback($params);
 
       $node->get('field_event_feedback')
-        ->appendItem(array('target_id' => $new_page->id()));
+        ->appendItem(array('target_id' => $new_feedback->id()));
       $node->save();
 
       $response['response'] = '1';
       $response['response_text'] = $this->t("Feedback added for @asset", [
         '@asset' => $node->getTitle()
       ]);
-    } else {
+    }
+    else {
       $response['response'] = '0';
       $response['response_text'] = $this->t("Could not find event with id @id", [
         '@id' => $id
@@ -77,39 +67,76 @@ class FeedbackController extends ControllerBase {
     if ($node && $node->getType() == 'asset') {
 
       $params = $request->request->all();
+      $this->feedback_check_required_fields($response, $params);
 
-      $new_page_values = array(
-        'type' => 'feedback',
-        'title' => 'feedback_' . time(),
-        'field_feedback_feedback' => array(
-          'value' => $params['feedback'],
-          'format' => 'basic_html',
-        ),
-        'field_feedback_user' => array(
-          'value' => $params['user'],
-        ),
-        'field_feedback_rating' => array(
-          'value' => $params['rating'],
-        ),
-      );
+      if (!empty($response)) {
+        return new JsonResponse($response);
+      }
 
-      $new_page = entity_create('node', $new_page_values);
-      $new_page->save();
+      $new_feedback = $this->feedback_create_feedback($params);
 
       $node->get('field_asset_feedback')
-        ->appendItem(array('target_id' => $new_page->id()));
+        ->appendItem(array('target_id' => $new_feedback->id()));
       $node->save();
 
       $response['response'] = '1';
       $response['response_text'] = $this->t("Feedback added for @asset", [
         '@asset' => $node->getTitle()
       ]);
-    } else {
+    }
+    else {
       $response['response'] = '0';
       $response['response_text'] = $this->t("Could not find asset with id @id", [
         '@id' => $id
       ]);
     }
     return new JsonResponse($response);
+  }
+
+  private function feedback_check_required_fields($response, $params) {
+    if (empty($params['feedback'])) {
+      $response['response'] = '0';
+      $response['response_text'] = $this->t("Field feedback required");
+    }
+    if (empty($params['user'])) {
+      $response['response'] = '0';
+      $response['response_text'] = $this->t("Field user required");
+    }
+    if (!isset($params['rating'])) {
+      $response['response'] = '0';
+      $response['response_text'] = $this->t("Field rating required");
+    }
+    else {
+      if (!is_numeric($params['rating'])) {
+        $response['response'] = '0';
+        $response['response_text'] = $this->t("Rating should be a number");
+      }
+    }
+    return $response;
+  }
+
+  /**
+   * @param $params
+   * @return \Drupal\Core\Entity\EntityInterface
+   */
+  public function feedback_create_feedback($params) {
+    $new_feedback_values = array(
+      'type' => 'feedback',
+      'title' => 'feedback_' . time(),
+      'field_feedback_feedback' => array(
+        'value' => $params['feedback'],
+        'format' => 'basic_html',
+      ),
+      'field_feedback_user' => array(
+        'value' => $params['user'],
+      ),
+      'field_feedback_rating' => array(
+        'value' => $params['rating'],
+      ),
+    );
+
+    $new_feedback = entity_create('node', $new_feedback_values);
+    $new_feedback->save();
+    return $new_feedback;
   }
 }
